0bfe7f749c4deab641e24da310b9ed0516612182
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-07-20 17:38:48 +0200

    hl3gxx: delete the GSM stuff, why do we need it?

--- omni_hl3gxx.mk
+++ omni_hl3gxx.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
